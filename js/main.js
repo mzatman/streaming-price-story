ee552023cfd3f11a3d8356c91ac89e6e90996c86
molly <90348734+mzatman@users.noreply.github.com>
2026-03-26 23:03:50 -0400

    Add platform navigation functionality

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ function login(event) {
   event.preventDefault();
   window.location.href = "pages/dashboard.html";
 }
+function goToPlatform(name) {
+  localStorage.setItem("platform", name);
+  window.location.href = "platform.html";
+}
+
+window.onload = function() {
+  const platform = localStorage.getItem("platform");
+  if (platform) {
+    document.getElementById("platformTitle").innerText = platform.toUpperCase();
+  }
+};
